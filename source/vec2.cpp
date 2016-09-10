daca4c123149ca4186a7da12a7e704aa6dc5d35f
= <=>
2016-09-10 17:34:48 +0200

    Vec2 ready

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -11,37 +11,53 @@ x(x_),
 y(y_)
 {}
 
+float Vec2::getx() const {
+	return x;
+}
+
+float Vec2::gety() const {
+	return y;
+}
+
 Vec2& Vec2::operator+=( Vec2 const& v ){
-	return new Vec2(x+v.x, y+v.y);
+	x += v.getx();
+	y += v.gety();
+	return *this;
 }
 
 Vec2& Vec2::operator-=( Vec2 const& v ) {
-	return new Vec2(x-v.x, y-v.y);
+	x -= v.getx();
+	y -= v.gety();
+	return *this;
 }
 
 Vec2& Vec2::operator*=( float v ) {
-	return new Vec2(x*v, y*v);
+	x *= v;
+	y *= v;
+	return *this;
 }
 
 Vec2& Vec2::operator/( float v ) {
-	return new Vec2(x/v, y/v);
+	x /= v;
+	y /= v;
+	return *this;
 }
 
-Vec2 Vec2::operator+( Vec2 const& u , Vec2 const& v ) {
-	return Vec2(u.x+v.x, u.y+v.y);
+Vec2 operator+( Vec2 const& u , Vec2 const& v ) {
+	return Vec2(u.getx()+v.getx(), u.gety()+v.gety());
 }
 
-Vec2 Vec2::operator-( Vec2 const& u , Vec2 const& v ) {
-	return Vec2(u.x-v.x, u.y-v.y);
+Vec2 operator-( Vec2 const& u , Vec2 const& v ) {
+	return Vec2(u.getx()-v.getx(), u.gety()-v.gety());
 }
 
-Vec2 Vec2::operator*( Vec2 const& v , float s ) {
-	return Vec2(v.x*s, v.y*s);
+Vec2 operator*( Vec2 const& v , float s ) {
+	return Vec2(v.getx()*s, v.gety()*s);
 }
 
-Vec2 Vec2::operator/( Vec2 const& v , float s ) {
-	return Vec2(v.x/s, v.y/s);
+Vec2 operator/( Vec2 const& v , float s ) {
+	return Vec2(v.getx()/s, v.gety()/s);
 }
-Vec2 Vec2::operator*( float s , Vec2 const& v ) {
-	return Vec2(v.x*s, v.y*s);
+Vec2 operator*( float s , Vec2 const& v ) {
+	return Vec2(v.getx()*s, v.gety()*s);
 }
--- source/vec2.hpp
+++ source/vec2.hpp
@@ -10,16 +10,20 @@ public:
 	Vec2& operator-=( Vec2 const& v );
 	Vec2& operator*=( float v );
 	Vec2& operator/( float v );
-	Vec2 operator+( Vec2 const& u , Vec2 const& v );
-	Vec2 operator-( Vec2 const& u , Vec2 const& v );
-	Vec2 operator*( Vec2 const& v , float s );
-	Vec2 operator/( Vec2 const& v , float s );
-	Vec2 operator*( float s , Vec2 const& v );
+	float getx() const;
+	float gety() const;
 
 private:
 	float x;
 	float y;
 	
 };
+
+Vec2 operator+( Vec2 const& u , Vec2 const& v );
+Vec2 operator-( Vec2 const& u , Vec2 const& v );
+Vec2 operator*( Vec2 const& v , float s );
+Vec2 operator/( Vec2 const& v , float s );
+Vec2 operator*( float s , Vec2 const& v );
+
 
 #endif
