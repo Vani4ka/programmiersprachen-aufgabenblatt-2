d721cffc575afac07ddded8b330a5eef293b1be3
= <=>
2016-09-15 13:28:45 +0200

    Corrected Mat2 and tests added

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -32,19 +32,21 @@ float Mat2::gety2() const {
 }
 
 Mat2& Mat2::operator *=( Mat2 const& m ) {
-	x1_=x1_*m.getx1() + x2_*m.gety1();
-	x2_=x1_*m.getx2() + x2_*m.gety2();
-	y1_=y1_*m.getx1() + y2_*m.gety1();
-	y2_=y1_*m.getx2() + y2_*m.gety2();
+	int x1=x1_*m.getx1() + x2_*m.gety1();
+	int x2=x1_*m.getx2() + x2_*m.gety2();
+	int y1=y1_*m.getx1() + y2_*m.gety1();
+	int y2=y1_*m.getx2() + y2_*m.gety2();
+	x1_=x1;
+	x2_=x2;
+	y1_=y1;
+	y2_=y2;
 	return *this;
 }
 
 Mat2 operator*( Mat2 const& m1 , Mat2 const& m2 ) {
-	float x1=m1.getx1()*m2.getx1() + m1.getx2()*m2.gety1();
-	float x2=m1.getx1()*m2.getx2() + m1.getx2()*m2.gety2();
-	float y1=m1.gety1()*m2.getx1() + m1.gety2()*m2.gety1();
-	float y2=m1.gety1()*m2.getx2() + m1.gety2()*m2.gety2();
-	return Mat2(x1, x2, y1, y2);
+	Mat2 tmp(m1);
+ 	tmp *= m2;
+ 	return tmp;
 }
 
 //Ohne Aufgabe 2.6
--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,32 @@
 #define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
+#include "catch.hpp"
+#include "mat2.hpp"
 
-int main(int argc, char *argv[])
+
+
+TEST_CASE ("describe_mat2","[mat2]")
 {
-  return Catch::Session().run(argc, argv);
+	Mat2 m1{1,2,3,4};
+	Mat2 m2{5,6,7,8};
+	m1*=m2;
+
+	Mat2 m3{5,6,7,8};
+	Mat2 m4{1,2,3,4};
+	Mat2 m5=m3*m4;
+
+
+	REQUIRE(m1.getx1()==19);
+	REQUIRE(m1.getx2()==22);
+	REQUIRE(m1.gety1()==43);
+	REQUIRE(m1.gety2()==50);
+
+	REQUIRE(m5.getx1()==23);
+	REQUIRE(m5.getx2()==34);
+	REQUIRE(m5.gety1()==31);
+	REQUIRE(m5.gety2()==46);
+}
+
+int main(int argc, char* argv[])
+{
+	return Catch::Session().run(argc, argv);
 }
